66ab8533a3059fc8de3b72a273fb1d878bf16cbf
tnicacio <nicacio.t92@gmail.com>
2021-02-20 18:42:51 -0300

    adjusting tables.sql formatation

--- tables/tables.sql
+++ tables/tables.sql
@@ -1,67 +1,67 @@
 create table usuario(
-id				serial primary key,
-nome			varchar(255),
-email			varchar(255),
-senha			varchar(255)
+id serial primary key,
+nome varchar(255),
+email varchar(255),
+senha varchar(255)
 );
 
 create table produto(
-id 					serial primary key,
-descricao			varchar(255),
-preco				decimal(6,2),
-qt_estoque			decimal(6,2),
-qt_estoque_minimo	decimal(6,2),
-qt_reservada		decimal(6,2),
-dt_atualizacao		timestamp,
-usuario_id			int4 not null
+id serial primary key,
+descricao varchar(255),
+preco decimal(6,2),
+qt_estoque decimal(6,2),
+qt_estoque_minimo decimal(6,2),
+qt_reservada decimal(6,2),
+dt_atualizacao timestamp,
+usuario_id int4 not null
 );
 
 create table venda(
-id				serial primary key,
-dt_venda		timestamp,
-is_pagto_prazo	boolean,
-nr_parcelas		int4,
-status			char,
-dt_atualizacao	timestamp,
-usuario_id		int4 not null
+id serial primary key,
+dt_venda timestamp,
+is_pagto_prazo boolean,
+nr_parcelas int4,
+status char,
+dt_atualizacao timestamp,
+usuario_id int4 not null
 );
 
 create table itens(
-produto_id		int4 not null,
-venda_id		int4 not null,
-qt_produto		decimal(6,2),
-preco_unitario	decimal(6,2),
+produto_id int4 not null,
+venda_id int4 not null,
+qt_produto decimal(6,2),
+preco_unitario decimal(6,2),
 primary key(produto_id, venda_id)
 );
 
 create table parcelamento(
-id				serial primary key,
-venda_id		int4 not null,
-valor			decimal(6,2),
-dt_vencimento	date
+id serial primary key,
+venda_id int4 not null,
+valor decimal(6,2),
+dt_vencimento date
 );
 
 create table produto_audit(
-id 					serial primary key,
-produto_id			int4 not null,
-usuario_id			int4 not null,
-dt_atualizacao		timestamp,
-descricao			varchar(255),
-preco				decimal(6,2),
-qt_estoque_minimo	decimal(6,2)
+id serial primary key,
+produto_id int4 not null,
+usuario_id int4 not null,
+dt_atualizacao timestamp,
+descricao varchar(255),
+preco decimal(6,2),
+qt_estoque_minimo decimal(6,2)
 );
 
 create table log_movimentos(
-id						serial primary key,
-produto_id				int4 not null,
-venda_id				int4,
-dt_atualizacao			timestamp,
-ds_movimento			varchar(255),
-qt_estoque_anterior		decimal(6,2),
-qt_estoque_atual		decimal(6,2),
-qt_reservada_anterior	decimal(5,2),
-qt_reservada_atual		decimal(6,2),
-usuario_id				int4 not null
+id serial primary key,
+produto_id int4 not null,
+venda_id int4,
+dt_atualizacao timestamp,
+ds_movimento varchar(255),
+qt_estoque_anterior decimal(6,2),
+qt_estoque_atual decimal(6,2),
+qt_reservada_anterior decimal(5,2),
+qt_reservada_atual decimal(6,2),
+usuario_id int4 not null
 );
 
 alter table produto
